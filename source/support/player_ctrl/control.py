6d16bc670d90f1021075fbd6e2115d9bb2561711
GuilhermeGS <ggsartori@inf.ufrgs.br>
2019-12-07 18:29:33 -0300

    #7 Basic youtube automation using pyautogui

--- source/support/player_ctrl/control.py
+++ source/support/player_ctrl/control.py
@@ -1,8 +1,18 @@
+import pyautogui
+
+# pretty basic interaction
+# PRE-CONDITIONS:
+    # Youtube opened on the video
+    # Video window actived as the current window
+
 def fist():
     print("play")
+    pyautogui.press('space')
 
 def bup():
     print("volume up")
+    pyautogui.press('up')
 
 def sup():
     print("volume down")
+    pyautogui.press('down')
